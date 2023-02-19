735621f6bd362f0c35b74d1e5cc4a6bd0cd2686b
Tushar-Bhatia-21 <113343003+Tushar-Bhatia-21@users.noreply.github.com>
2023-02-19 22:23:37 +0530

    Update 14.02.23.c

--- College/OS/Lab/14.02.23.c
+++ College/OS/Lab/14.02.23.c
@@ -79,3 +79,50 @@ int main()
 
 
 //SRTF
+#include <stdio.h>
+
+int main() {
+    int n, time = 0, shortest_time, current_process, exit_time;
+    float avg_wait_time = 0, avg_turnaround_time = 0;
+    //Process Input
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    int pid[n], arrival_time[n], burst_time[n], remaining_time[n], completed[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival time and burst time of process %d: ", i + 1);
+        scanf("%d %d", &arrival_time[i], &burst_time[i]);
+        pid[i] = i + 1;
+        remaining_time[i] = burst_time[i];
+    }
+// SRTF function to calculate avg wait time and avg turn around time
+    int num_completed = 0; // It keeps  the track of the number of processess completed
+    while (num_completed < n) {
+        shortest_time = -1;
+        for (int i = 0; i < n; i++) {
+            if (arrival_time[i] <= time && remaining_time[i] > 0) {
+                if (shortest_time == -1 || remaining_time[i] < remaining_time[current_process]) {
+                    current_process = i;
+                    shortest_time = remaining_time[i];
+                }
+            }
+        }
+        remaining_time[current_process]--;
+        if (remaining_time[current_process] == 0) {
+            completed[current_process] = time + 1;
+            num_completed++;
+            avg_wait_time += completed[current_process] - arrival_time[current_process] - burst_time[current_process];
+            avg_turnaround_time += completed[current_process] - arrival_time[current_process];
+        }
+        time++;
+    }
+    //Printing the PID, Arrival time, Burst time, Exit time, Waiting time and Turnaround time
+    printf("PID\tArrival Time\tBurst Time\tExit Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", pid[i], arrival_time[i], burst_time[i], completed[i],
+               completed[i] - arrival_time[i] - burst_time[i], completed[i] - arrival_time[i]);
+    }
+    printf("Average waiting time: %.2f\n", avg_wait_time / n);
+    printf("Average turnaround time: %.2f\n", avg_turnaround_time / n);
+    return 0;
+}
+
